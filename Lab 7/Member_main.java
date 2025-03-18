57c5440e9cae7a882caaf30172cf3ac01f837d72
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-03-18 08:06:33 +0530

    update

--- Lab 7/Member_main.java
+++ Lab 7/Member_main.java
@@ -26,31 +26,32 @@ class Employee extends Member {
     Employee(String name, int age, int phonenumber, String address, double salary) {
         super(name, age, phonenumber, address, salary);
     }
-    void display(){
-        System.out.println(name + " " +age+ " " + phonenumber + " " + address + " " +specialization);
+
+    void display() {
+        System.out.println(name + " " + age + " " + phonenumber + " " + address + " " + specialization);
         super.printSalary();
     }
 }
 
-class Manager extends Member{
+class Manager extends Member {
     String department = "IT";
-    Manager(String name, int age, int phonenumber, String address, double salary)  {
+
+    Manager(String name, int age, int phonenumber, String address, double salary) {
         super(name, age, phonenumber, address, salary);
     }
-    void display(){
-        System.out.println(name + " " +age+ " " + phonenumber + " " + address + " " +department);
+
+    void display() {
+        System.out.println(name + " " + age + " " + phonenumber + " " + address + " " + department);
         super.printSalary();
     }
 }
-     
-    
 
 public class Member_main {
     public static void main(String[] args) {
-       
-       Employee e = new Employee("Hem", 25, 1234567890, "Puna", 200000000);
-       Manager m = new Manager("Nilesh", 45, 1111111111, "Rajkot", 999999999);
-       e.display();
-       m.display();
+
+        Employee e = new Employee("Hem", 25, 1234567890, "Puna", 200000000);
+        Manager m = new Manager("Nilesh", 45, 1111111111, "Rajkot", 999999999);
+        e.display();
+        m.display();
     }
 }
---
+++ Lab 7/Super_keyword.java
@@ -0,0 +1,20 @@
+class A{
+    int a = 10;
+    A(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+public class Super_keyword {
+    public static void main(String[] args) {
+        
+    }
+}
